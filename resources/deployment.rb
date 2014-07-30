e1288c1d3853a782e24ba9fd4af90eb7d1c90b5a
Soulou <leo@unbekandt.eu>
2014-07-30 11:15:01 +0200

    Fix resources keys

--- resources/deployment.rb
+++ resources/deployment.rb
@@ -14,8 +14,8 @@ default_action :notify
 attribute :path, :kind_of => String, :name_attribute => true
 attribute :token, :kind_of => String, :required => true
 attribute :env, :kind_of => String, :required => true
-attribute :local_username, kind_of => String, :required => true
+attribute :local_username, :kind_of => String, :required => true
 # Revision is defined from the checkout rev in the given path
-attribute :revision, kind_of => String
-attribute :rollbar_username, kind_of => String
-attribute :comment, kind_of => String
+attribute :revision, :kind_of => String
+attribute :rollbar_username, :kind_of => String
+attribute :comment, :kind_of => String
